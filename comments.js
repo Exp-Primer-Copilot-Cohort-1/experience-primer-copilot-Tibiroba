c6ff7e2f65840b7389d992de4530130fcd03718e
Ricardo Paulussi <49121352+Tibiroba@users.noreply.github.com>
2024-02-12 13:20:41 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,24 +1,45 @@
-
-// Create a Web Server
+// Create a web server
+//
 var http = require('http');
-
-// File System
+var url = require('url');
 var fs = require('fs');
+var qs = require('querystring');
+var comments = [];
 
-// Create Server
-http.createServer(function(req, res) {
-  // Read file
-  fs.readFile('./index.html', function(err, data) {
-    // Check for errors
-    if (err) {
-      console.log(err);
-      res.writeHead(404);
-      res.end('Sorry, the file could not be found');
-    }
-    // Send the file
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end(data);
-  });
-}).listen(1337, '
+var server = http.createServer(function(req, res) {
+  // Parse the request containing file name
+  var pathname = url.parse(req.url).pathname;
 
+  if (pathname === '/') {
+    fs.readFile('index.html', function(err, data) {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.write(data);
+      res.end();
+    });
+  } else if (pathname === '/post') {
+    var body = '';
+    req.on('data', function(data) {
+      body += data;
+      if (body.length > 1e6) {
+        req.connection.destroy();
+      }
+    });
+    req.on('end', function() {
+      var post = qs.parse(body);
+      comments.push(post.comment);
+      res.writeHead(302, { 'Location': '/' });
+      res.end();
+    });
+  } else if (pathname === '/comments') {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.write(JSON.stringify(comments));
+    res.end();
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/html' });
+    res.write('404 Not found');
+    res.end();
+  }
+});
 
+server.listen(8080);
+console.log('Server running at http://
